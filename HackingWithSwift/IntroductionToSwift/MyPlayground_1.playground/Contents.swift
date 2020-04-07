efcd7e59379f9c0da8aaec150a3cc94fe21e3751
mrstevenaweiss <weiss.steven@gmail.com>
2020-04-06 23:28:31 -0400

    added project1

--- HackingWithSwift/IntroductionToSwift/MyPlayground_1.playground/Contents.swift
+++ HackingWithSwift/IntroductionToSwift/MyPlayground_1.playground/Contents.swift
@@ -1,3 +1,29 @@
-import UIKit
+//import UIKit
+//
+//var str = "Hello, playground"
+//
+//enum Activity {
+//    case bored
+//    case running(destination: String)
+//    case talking(topic: String)
+//    case singing(volume: Int)
+//}
+//
+//var item = Activity.bored
+//print(item)
+//
+//item = Activity.talking(topic: "politics")
+//print(item)
 
-var str = "Hello, playground"
+
+let driving = {
+    print("I'm driving in my car")
+}
+
+func travel(action: () -> Void) {
+    print("I'm getting ready to go.")
+    action()
+    print("I arrived!")
+}
+
+travel(action: driving )
---
+++ HackingWithSwift/Projects/Project1/Project1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by steven weiss on 4/6/20.
+//  Copyright © 2020 Mr Steven A Weiss. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
