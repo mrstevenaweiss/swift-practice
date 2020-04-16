ee77381bd80b902a228fa916019f7d8e00432e6a
mrstevenaweiss <weiss.steven@gmail.com>
2020-04-15 22:21:51 -0400

    hey

--- HackingWithSwift/Projects/Project1/Project1/ViewController.swift
+++ HackingWithSwift/Projects/Project1/Project1/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  Project1
+    //  Project1
 //
 //  Created by steven weiss on 4/6/20.
 //  Copyright © 2020 Mr Steven A Weiss. All rights reserved.
@@ -8,13 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    var pictures = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+
+        for item in items {
+            if item.hasPrefix("nssl") {
+                // this is a picture to load!
+                pictures.append(item)
+            }
+        }
+        print(pictures)
     }
-
-
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
 }
 
