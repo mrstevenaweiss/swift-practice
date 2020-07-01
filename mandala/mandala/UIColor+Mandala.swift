c7e7995829581436aa02a2e672da1952d6548e8c
mrstevenaweiss <weiss.steven@gmail.com>
2020-06-30 22:01:25 -0400

    wrote mad extensions

--- mandala/mandala/UIColor+Mandala.swift
+++ mandala/mandala/UIColor+Mandala.swift
@@ -6,4 +6,15 @@
 //  Copyright © 2020 Mr Steven A Weiss. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UIColor {
+    static let angry = UIColor(named: "angryRed")!
+    static let confused = UIColor(named: "confusedPurple")!
+    static let crying = UIColor(named: "cryingLightBlue")!
+    static let goofy = UIColor(named: "goofyOrange")!
+    static let happy = UIColor(named: "happyTurquoise")!
+    static let meh = UIColor(named: "mehGray")!
+    static let sad = UIColor(named: "sadBlue")!
+    static let sleepy = UIColor(named: "sleepyLightRed")!
+}
--- mandala/mandala/UIImage+Mandala.swift
+++ mandala/mandala/UIImage+Mandala.swift
@@ -6,4 +6,23 @@
 //  Copyright © 2020 Mr Steven A Weiss. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+enum ImageResource: String {
+    case angry
+    case confused
+    case crying
+    case goofy
+    case happy
+    case meh
+    case sad
+    case sleepy
+}
+
+
+extension UIImage {
+    convenience init(resource: ImageResource) {
+        self.init(named: resource.rawValue)!
+    }
+    
+}
